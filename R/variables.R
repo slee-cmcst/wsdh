dc04a0e398449f2cbe32c42d8c7edb37af2174e4
Lee <slee177@cable.comcast.com>
2025-09-08 15:09:36 -0500

    make setting database an active function call

--- R/variables.R
+++ R/variables.R
@@ -1,30 +1,4 @@
-# set database dir
-# this depends on when the user's computer was flashed by Comcast
-# try this first
-database_root_dir <- file.path(
-        "C:\\",
-        "Users",
-        Sys.getenv("USERNAME"),
-        "OneDrive - Comcast",
-        "Central Division People Analytics - Database"
-    )
-
-if (!dir.exists(database_root_dir)) {
-    database_dir <- file.path(
-        "C:\\",
-        "Users",
-        paste0(
-            Sys.getenv("USERNAME"),
-            "@cable.comcast.com"
-        ),
-        "OneDrive - Comcast",
-        "Central Division People Analytics - Database"
-    )
-    if (!dir.exists(database_root_dir)) {
-        cat("Cannot find the Database directory on your machine.")
-    }
-}
-
+database_root_dir <- get_database_dir()
 database_dir <- file.path(database_root_dir, "Clean")
 
 datasets_list <- list(
