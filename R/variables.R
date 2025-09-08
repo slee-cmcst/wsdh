d385bc0f4739d1fa93fa8536f8aefa1f1dcaffa5
Lee <slee177@cable.comcast.com>
2025-09-08 14:46:14 -0500

    update file path to handle differnt computer settings

--- R/variables.R
+++ R/variables.R
@@ -1,11 +1,31 @@
-database_dir <- file.path(
+# set database dir
+# this depends on when the user's computer was flashed by Comcast
+# try this first
+database_root_dir <- file.path(
         "C:\\",
         "Users",
         Sys.getenv("USERNAME"),
         "OneDrive - Comcast",
         "Central Division People Analytics - Database",
-        "Clean"
     )
+
+if (!dir.exists(database_root_dir)) {
+    database_dir <- file.path(
+        "C:\\",
+        "Users",
+        paste0(
+            Sys.getenv("USERNAME"),
+            "@cable.comcast.com"
+        ),
+        "OneDrive - Comcast",
+        "Central Division People Analytics - Database",
+    )
+    if (!dir.exists(database_root_dir)) {
+        cat("Cannot find the Database directory on your machine.")
+    }
+}
+
+database_dir <- file.path(database_root_dir, "Clean")
 
 datasets_list <- list(
     "subscribers" = file.path("Business", "subscribers.csv"),
