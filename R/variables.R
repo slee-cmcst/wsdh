6db0257a8d69a514f7bd3cd3e1f9de4228993fcb
Lee <slee177@cable.comcast.com>
2025-09-10 15:52:00 -0500

    add retail data

--- R/variables.R
+++ R/variables.R
@@ -4,5 +4,6 @@ database_dir <- file.path(database_root_dir, "Clean")
 datasets_list <- list(
     "subscribers" = file.path("Business", "subscribers.csv"),
     "homes_passed" = file.path("Business", "homes_passed.csv"),
-    "competitive_intelligence" = file.path("Business", "competitive_intelligence.csv")
+    "competitive_intelligence" = file.path("Business", "competitive_intelligence.csv"),
+    "retail_store_performance" = file.path("Business", "Retail", "retail_store_performance.csv")
 )
